06e5196561c3d5f035984dce04104ec68653a8bb
Naveen <naveentruly86@gmail.com>
2021-06-21 14:22:39 +0530

    Removing Avoidable Words From Paragraph

--- src/main/java/com/bridgelabz/LinkedHashMap.java
+++ src/main/java/com/bridgelabz/LinkedHashMap.java
@@ -27,7 +27,7 @@ public class LinkedHashMap<K, V> {
         if(linkedList == null) {
             return null;
         }
-        MapNode<K, V> mapNode = (MapNode<K, V>) linkedList.such(key);
+        MapNode<K, V> mapNode = (MapNode<K, V>) linkedList.search(key);
         return (mapNode == null) ? null : mapNode.getValue();
     }
 
@@ -38,13 +38,39 @@ public class LinkedHashMap<K, V> {
             linkedList = new LinkedList<> ();
             this.bucketArray.set(index, linkedList);
         }
-        MapNode<K, V> mapNode = (MapNode<K,V>) linkedList.such(key);
+        MapNode<K, V> mapNode = (MapNode<K,V>) linkedList.search(key);
         if (mapNode == null) {
             mapNode = new MapNode<>(key, value);
-            linkedList.attach(mapNode);
+            linkedList.append(mapNode);
         } else {
             mapNode.setValue(value);
         }
+    }
+
+    public V remove(K key) {
+        int index = this.getBucketIndex(key);
+        LinkedList<K> newLinkedList = this.bucketArray.get(index);
+
+        MapNode<K,V> headNode = (MapNode<K, V>) newLinkedList.search(key);
+
+        MapNode<K, V> prev = null;
+        while (headNode != null) {
+            if (headNode.key.equals(key))
+                break;
+
+            prev = headNode;
+            headNode = headNode.next;
+        }
+
+        if (headNode == null)
+            return null;
+
+        if (prev != null)
+            prev.next = headNode.next;
+        else
+            bucketArray.set(index, headNode.next);
+
+        return headNode.value;
     }
 
     @Override
